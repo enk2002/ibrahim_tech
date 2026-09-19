4e8ff560c1bfc10e5429439698153d907172d0f7
enk2002 <nkamadaemmanuel25@gmail.com>
2026-09-19 12:33:26 +0000

    Update logo size in CSS, add new logo image, and modify contact form visibility

--- forms/contact.php
+++ forms/contact.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <!-- <?php -->
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
@@ -7,16 +7,16 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // $receiving_email_address = 'nkamadaemmanuel25@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  // }
 
   $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  $contact->ajax = false;
   
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
@@ -38,4 +38,4 @@
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
-?>
+?> -->
